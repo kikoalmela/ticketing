48a63e9c15ba02cdcd603b88fd2a0c8857c4325d
kikoalmela <kiko@kikoalmela.com>
2020-05-13 14:25:43 +0200

    reat(user): define extra document properties

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -9,8 +9,15 @@ interface UserAttrs {
 
 // An interface that describes the properties
 // that a User Model has
-interface UserModel extends mongoose.Model<any> {
-  build(attrs: UserAttrs): any;
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+// An interface that describes the properties
+// that a User Document has
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
 }
 
 const userSchema = new mongoose.Schema({
@@ -26,6 +33,5 @@ const userSchema = new mongoose.Schema({
 
 userSchema.statics.build = (attrs: UserAttrs) => new User(attrs);
 
-const User = mongoose.model<any, UserModel>('User', userSchema);
-
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
 export { User };
