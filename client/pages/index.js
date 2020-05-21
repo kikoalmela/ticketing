3209210a7f59f6fe7f2026f3307d02631c02f154
kikoalmela <kiko@kikoalmela.com>
2020-05-21 17:26:27 +0200

    feat(app): move getInitialProps to app component and handle multiple getInitialProps

--- client/pages/index.js
+++ client/pages/index.js
@@ -4,6 +4,7 @@ const LandingPage = ({ currentUser }) =>
   currentUser ? <h1>You are signed in</h1> : <h1>You are NOT signed in</h1>;
 
 LandingPage.getInitialProps = async (context) => {
+  console.log('LANDING PAGE!');
   const client = buildClient(context);
   const { data } = await client.get('/api/users/currentuser');
 
