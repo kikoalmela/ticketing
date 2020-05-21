cc1b2a0aed60da5158875f75c1cbfea70a8744cb
kikoalmela <kiko@kikoalmela.com>
2020-05-21 16:15:56 +0200

    feat: get current user through initial props from server on loading

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,1 +1,37 @@
-export default () => <h1>Landing Page!</h1>;
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+
+  return <h1>Landing Page!</h1>;
+};
+
+LandingPage.getInitialProps = async () => {
+  // const response = await axios.get('http.//ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser');
+
+  // return response.data;
+
+  if (typeof window === 'undefined') {
+    // we are on the server
+    // requests should be made with a base url of "http://SERVICENAME.NAMESPACE.svc.cluster.local/"
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: {
+          Host: 'ticketing.dev',
+        },
+      }
+    );
+
+    return data;
+  } else {
+    // we are on the browser
+    // requests should be made with a base url of ''
+    const { data } = await axios.get('/api/users/currentuser');
+
+    return data;
+  }
+  return {};
+};
+
+export default LandingPage;
