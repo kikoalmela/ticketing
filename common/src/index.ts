a3825c53ace51cbdf6b65374fa463fcdad79e079
kikoalmela <kiko@kikoalmela.com>
2020-05-22 16:21:35 +0200

    feat(auth): delete and import from common dependency the code extracted to it

---
+++ common/src/index.ts
@@ -0,0 +1,12 @@
+// Re-export from index to make import from this dependency easier
+export * from './errors/bad-request-error';
+export * from './errors/custom-error';
+export * from './errors/database-connection-error';
+export * from './errors/not-authorized-error';
+export * from './errors/not-found-error';
+export * from './errors/request-validation-error';
+
+export * from './middlewares/current-user';
+export * from './middlewares/error-handler';
+export * from './middlewares/require-auth';
+export * from './middlewares/validate-request';
