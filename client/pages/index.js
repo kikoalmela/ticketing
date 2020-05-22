768664b718df3ca2f3b959104e145e16c58fc632
kikoalmela <kiko@kikoalmela.com>
2020-05-22 12:49:28 +0200

    feat(header): show links conditionally

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,12 @@
 import buildClient from '../api/build-client';
 
-const LandingPage = ({ currentUser }) =>
-  currentUser ? <h1>You are signed in</h1> : <h1>You are NOT signed in</h1>;
+const LandingPage = ({ currentUser }) => {
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are NOT signed in</h1>
+  );
+};
 
 LandingPage.getInitialProps = async (context) => {
   console.log('LANDING PAGE!');
