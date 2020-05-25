53b515e766920bc58ecb675370380e59c0c63c81
kikoalmela <kiko@kikoalmela.com>
2020-05-25 14:21:21 +0200

    feat(tickets): setup tickets service

---
+++ tickets/src/app.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError } from '@katickets/common';
+
+export const app = express();
+app.use(json());
+app.set('trust proxy', true);
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+
+app.all('*', async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
