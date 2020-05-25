27f7d2d145bf77fbfb22503ecac50b70219e6eb2
kikoalmela <kiko@kikoalmela.com>
2020-05-25 15:45:55 +0200

    feat(tickets): create router

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -4,6 +4,8 @@ import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError } from '@katickets/common';
 
+import { createTicketRouter } from './routes/new';
+
 export const app = express();
 app.use(json());
 app.set('trust proxy', true);
@@ -13,6 +15,8 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
+
+app.use(createTicketRouter);
 
 app.all('*', async () => {
   throw new NotFoundError();
