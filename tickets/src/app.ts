c1e917e01889648350c829e6b7eaeb8b336539d4
kikoalmela <kiko@kikoalmela.com>
2020-05-26 15:39:54 +0200

    feat(tickets): handle update route

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -7,6 +7,7 @@ import { errorHandler, NotFoundError, currentUser } from '@katickets/common';
 import { createTicketRouter } from './routes/new';
 import { showTicketRouter } from './routes/show';
 import { indexTicketRouter } from './routes';
+import { updateTicketRouter } from './routes/update';
 
 export const app = express();
 app.use(json());
@@ -22,6 +23,7 @@ app.use(currentUser);
 app.use(createTicketRouter);
 app.use(showTicketRouter);
 app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 app.all('*', async () => {
   throw new NotFoundError();
